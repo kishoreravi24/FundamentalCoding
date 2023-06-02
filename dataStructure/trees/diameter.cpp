368f44fb451d7a732b3544e90e4c3d2d0af2ec7b
kishore Jr <kishoreravi24399@gmail.com>
2023-06-02 06:38:13 +0530

    2 jun 23

---
+++ dataStructure/trees/diameter.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    // Function to return the diameter of a Binary Tree.
+    int height(Node* root){
+        if(root == NULL){
+            return 0;
+        }
+        int lh = height(root->left);
+        int rh = height(root->right);
+        
+        return std::max(lh, rh) + 1;
+    }
+    
+    // Function to return the diameter of a Binary Tree.
+    int diameter(Node* root) {
+        if(root == NULL){
+            return 0;
+        }
+        
+        int lh = height(root->left);
+        int rh = height(root->right);
+        
+        int ld = diameter(root->left);
+        int rd = diameter(root->right);
+        
+        return std::max({ld, rd, lh + rh + 1});
+    }
+};
